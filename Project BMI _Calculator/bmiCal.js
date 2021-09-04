84f781d0f8e4cc52105233f64b8401fd9dc68e72
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-09-04 12:26:23 +0530

    Add files via upload

---
+++ Project BMI _Calculator/bmiCal.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+const express=require("express");
+const bodyParser=require("body-parser");
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/bmiCalculator.html");
+});
+
+app.post("/",function(req,res){
+  var weight=Number(req.body.weight);
+  var height=Number(req.body.height);
+  var result=(weight/(height*height));
+  res.send("The Calculated BMI is: "+result);
+});
+app.listen(3000,function(){
+  console.log("Mr. Ayush server is running on port 3000");
+});
